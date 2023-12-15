968d2ed5c5597a8ccb88e4fa48606b5e4abc1fa8
baswanthd <61455583+baswanthd@users.noreply.github.com>
2023-12-15 11:42:31 +0100

    Update ecs_task_defination.tf

--- ecs_task_defination.tf
+++ ecs_task_defination.tf
@@ -1,6 +1,6 @@
 resource "aws_ecs_task_definition" "baswanth-bmi-service" {
   family = "bmi-service"
-  container_definations = jsonencode([
+  container_definitions = jsonencode([
   {
    name = "bas-first"
    image = "124937358799.dkr.ecr.eu-west-2.amazonaws.com/devops_repro"
