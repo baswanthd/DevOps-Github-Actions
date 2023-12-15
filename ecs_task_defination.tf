bfc63d2a157c411bb689bcd506b55142fe8e48cd
baswanthd <61455583+baswanthd@users.noreply.github.com>
2023-12-15 12:01:59 +0100

    Update ecs_task_defination.tf

--- ecs_task_defination.tf
+++ ecs_task_defination.tf
@@ -1,5 +1,7 @@
 resource "aws_ecs_task_definition" "baswanth-bmi-td" {
   family = "bmi-service"
+  require_compatibilities = [ "FARGATE" ]
+  network_mode = "awsvpc"
   container_definitions = jsonencode([
   {
    name = "bas-first"
