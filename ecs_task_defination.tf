9de7e7d53f0a2da6b6b3ae4e3495cbe624647bd5
baswanthd <61455583+baswanthd@users.noreply.github.com>
2023-12-15 11:37:17 +0100

    Update ecs_task_defination.tf

--- ecs_task_defination.tf
+++ ecs_task_defination.tf
@@ -3,7 +3,7 @@ resource "aws_ecs_task_defination" "baswanth-bmi-service" {
   container_definations = jsonencode([
   {
    name = "bas-first"
-   image = 
+   image = "124937358799.dkr.ecr.eu-west-2.amazonaws.com/devops_repro"
    cpu = 0
    memory = 512
    essentials = true
