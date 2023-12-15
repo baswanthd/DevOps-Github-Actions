db65616cfe219c44e0fdc6a08761e5d9c8aa28d2
baswanthd <61455583+baswanthd@users.noreply.github.com>
2023-12-15 13:02:36 +0100

    update with working code

--- ecs_task_defination.tf
+++ ecs_task_defination.tf
@@ -1,24 +1,23 @@
-resource "aws_ecs_task_definition" "baswanth-bmi-td" {
-  family = "bmi-service-bas"
+resource "aws_ecs_task_definition" "bmi-service7" {
+  family = "bmi-service"
   requires_compatibilities = [ "FARGATE" ]
   network_mode = "awsvpc"
   cpu = 256
   memory = 512
-  execution_role_arn = "arn:aws:iam::124937358799:role/ecsTaskExecutionRole"
-  
+  execution_role_arn =  "arn:aws:iam::124937358799:role/ecsTaskExecutionRole"
   container_definitions = jsonencode([
     {
-      name = "bas-first"
-      image = "124937358799.dkr.ecr.eu-west-2.amazonaws.com/devops_repro"
-      cpu = 0
-      memory = 512
-      essentials = true
-      portMapping = [
+      name      = "first"
+      image     = "124937358799.dkr.ecr.eu-west-2.amazonaws.com/devops_repro"
+      cpu       = 0
+      memory    = 512
+      essential = true
+      portMappings = [
         {
           containerPort = 8000
-          hostPort = 8000
-       }
-  ]
-  }
-])
+          hostPort      = 8000
+        }
+      ]
+    }
+  ])
 }
