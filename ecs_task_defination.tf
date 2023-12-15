d59f3622a7b2e9c9d34ed7ffffbf98090381bbe4
baswanthd <61455583+baswanthd@users.noreply.github.com>
2023-12-15 12:55:10 +0100

    family name adjusted

--- ecs_task_defination.tf
+++ ecs_task_defination.tf
@@ -1,22 +1,23 @@
 resource "aws_ecs_task_definition" "baswanth-bmi-td" {
-  family = "bmiservice"
+  family = "bmi-service-bas"
   requires_compatibilities = [ "FARGATE" ]
   network_mode = "awsvpc"
   cpu = 256
   memory = 512
   execution_role_arn = "arn:aws:iam::124937358799:role/ecsTaskExecutionRole"
+  
   container_definitions = jsonencode([
-  {
-   name = "bas-first"
-   image = "124937358799.dkr.ecr.eu-west-2.amazonaws.com/devops_repro"
-   cpu = 0
-   memory = 512
-   essentials = true
-   portMapping = [
-   {
-     containerPort = 8000
-     hostPort = 8000
-   }
+    {
+      name = "bas-first"
+      image = "124937358799.dkr.ecr.eu-west-2.amazonaws.com/devops_repro"
+      cpu = 0
+      memory = 512
+      essentials = true
+      portMapping = [
+        {
+          containerPort = 8000
+          hostPort = 8000
+       }
   ]
   }
 ])
