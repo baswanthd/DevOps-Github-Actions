9c2e310271e54ab39bfa1546af62ccd8c26221b6
baswanthd <61455583+baswanthd@users.noreply.github.com>
2023-12-20 16:29:56 +0100

    Created terraform.tfvars

---
+++ terraform.tfvars
@@ -0,0 +1,1 @@
+subnets = [ "subnet-01480a618ac23bf02", "subnet-09d97b80d657b45c8", "subnet-00582281cf7d0e00c" ]
--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
 variable "subnets" {
-default = [ "subnet-01480a618ac23bf02", "subnet-09d97b80d657b45c8", "subnet-00582281cf7d0e00c" ]
+description = "subnets for FARGATE"
 }
 
