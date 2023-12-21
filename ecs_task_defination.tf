b14054434f3380c2cf8542baac23cfed2564ff77
baswanthd <61455583+baswanthd@users.noreply.github.com>
2023-12-22 00:02:40 +0100

    memory

--- ecs_task_defination.tf
+++ ecs_task_defination.tf
@@ -3,14 +3,14 @@ resource "aws_ecs_task_definition" "bmi-bas-service-tf" {
   requires_compatibilities = [ "FARGATE" ]
   network_mode = "awsvpc"
   cpu = 256
-  memory = 512
+  memory = 1024
   execution_role_arn =  "arn:aws:iam::124937358799:role/ecsTaskExecutionRole"
   container_definitions = jsonencode([
     {
       name      = "first"
       image     = "124937358799.dkr.ecr.eu-west-2.amazonaws.com/devops_repro"
       cpu       = 0
-      memory    = 512
+      memory    = 1024
       essential = true
       portMappings = [
         {
