86439b30b8d5443471b69307de639e486416aa18
baswanthd <61455583+baswanthd@users.noreply.github.com>
2023-12-21 23:36:52 +0100

    task definition

--- ecs_task_defination.tf
+++ ecs_task_defination.tf
@@ -1,5 +1,5 @@
 resource "aws_ecs_task_definition" "bmi-bas-service-tf" {
-  family = "bmi-service"
+  family = "devops-bmi1-taskdef"
   requires_compatibilities = [ "FARGATE" ]
   network_mode = "awsvpc"
   cpu = 256
