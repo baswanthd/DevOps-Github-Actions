7f61c82a15f029314e6b673ce9484024497da196
baswanthd <61455583+baswanthd@users.noreply.github.com>
2023-12-21 12:08:38 +0100

    Created AWS subnets with availability zones

--- variables.tf
+++ variables.tf
@@ -10,10 +10,14 @@ variable "vpc_cidr" {
     
 }
 
-variable "public_subnet_cidr" {
+variable "public_subnets_cidr" {
     
 }
 
-variable "private_subnet_cidr" {
+variable "private_subnets_cidr" {
     
+}
+
+variable "availability_zones" {
+    default = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
 }
--- vpc.tf
+++ vpc.tf
@@ -2,9 +2,21 @@ resource "aws_vpc" "main_vpc" {
   cidr_block = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support = true
-  
+
     tags = {
     Name = "main_vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnets" {
+  vpc_id     = aws_vpc.main_vpc.id
+  
+  count = length(var.public_subnets_cidr)
+  cidr_block = element(var.public_subnets_cidr, count.index)
+  availability_zone = element(var.availability_zones, count.index)
+
+  tags = {
+    Name = "public_subnet $(count.index + 1)"
   }
 }
 
