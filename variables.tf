1532e988e76ef6df3a080390600c5ea34b745048
baswanthd <61455583+baswanthd@users.noreply.github.com>
2023-12-21 11:50:42 +0100

    Created VPC and few variables

--- variables.tf
+++ variables.tf
@@ -2,3 +2,18 @@ variable "subnets" {
 description = "subnets for FARGATE"
 }
 
+variable "region" {
+    default = "eu-west-2"
+}
+
+variable "vpc_cidr" {
+    
+}
+
+variable "public_subnet_cidr" {
+    
+}
+
+variable "private_subnet_cidr" {
+    
+}
