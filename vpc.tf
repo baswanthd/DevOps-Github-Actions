d6a09905f5e0adc8b5475a6b206fbb39389b4b5d
baswanthd <61455583+baswanthd@users.noreply.github.com>
2023-12-22 23:21:28 +0100

    route table id

--- vpc.tf
+++ vpc.tf
@@ -101,7 +101,7 @@ resource "aws_route_table_association" "private" {
 resource "aws_route_table_association" "route_table_association" {
     count = length(var.public_subnets_cidr)
     subnet_id      = element(aws_subnet.public_subnets[*].id, count.index) 
-    route_table_id = aws_route_table.route_internet_gateway.id
+    route_table_id = element(aws_route_table.route_internet_gateway.*.id, count.index)
 }
 
 resource "aws_vpc_endpoint" "ecr-endpoint" {
