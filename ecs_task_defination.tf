3074c9a25c083388d3bf4c3d1f172a51eee2224a
baswanthd <61455583+baswanthd@users.noreply.github.com>
2023-12-22 21:48:16 +0100

    image

--- ecs_task_defination.tf
+++ ecs_task_defination.tf
@@ -8,7 +8,7 @@ resource "aws_ecs_task_definition" "bmi-bas-service-tf" {
   container_definitions = jsonencode([
     {
       name      = "first"
-      image     = "124937358799.dkr.ecr.eu-west-2.amazonaws.com/baswanthecr"
+      image     = "124937358799.dkr.ecr.eu-west-2.amazonaws.com/devops_repro"
       cpu       = 256
       memory    = 512
       essential = true
