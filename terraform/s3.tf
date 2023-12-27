8bdd7681cbe6540af79ec78a058c6117928dd7bf
snoppiz <46250679+snoppiz@users.noreply.github.com>
2023-12-27 13:56:34 +0100

    add policicy

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -3,32 +3,28 @@ resource "aws_s3_bucket" "log_bucket" {
 
 }
 
-resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+data "aws_elb_service_account" "log_account"{} 
+data "aws_caller_identity" "log_account_current" {}
+
+resource "aws_s3_bucket_policy" "log_data_policy" {
   bucket = aws_s3_bucket.log_bucket.id
   policy = data.aws_iam_policy_document.log_data.json
 }
 
 data "aws_iam_policy_document" "log_data" {
-  statement {
-    sid = "1"
+   statement {
+    principals {
+      type        = "AWS"
+      identifiers = [data.aws_elb_service_account.log_account.arn]
+    } 
 
     actions = [
-      "s3:ListAllMyBuckets",
-      "s3:GetBucketLocation",
-    ]
-
-    resources = [
-      "arn:aws:s3:::*",
-    ]
-  }
-
-  statement {
-    actions = [
-      "s3:ListBucket",
+      "s3:PutObject",
     ]
 
     resources = [
       aws_s3_bucket.log_bucket.arn,
+      "${aws_s3_bucket.log_bucket.arn}/${data.aws_caller_identity.log_account_current.account_id}/*",
     ]
   }
 }
