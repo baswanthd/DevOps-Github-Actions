ee41e4fef7e3ef0f8a09763e2a5751ac5cd254b9
snoppiz <46250679+snoppiz@users.noreply.github.com>
2023-12-27 13:16:19 +0100

    add documents policy

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,5 +1,29 @@
 resource "aws_s3_bucket" "log_bucket" {
   bucket = "log-bucket-ac-dev"
 
-  
+}
+
+data "aws_iam_policy_document" "log_data" {
+  statement {
+    sid = "1"
+
+    actions = [
+      "s3:ListAllMyBuckets",
+      "s3:GetBucketLocation",
+    ]
+
+    resources = [
+      "arn:aws:s3:::*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      data.aws_s3_bucket.log_bucket.arn,
+    ]
+  }
 }
