2933c5f36970cbbc444e12853e5d0203006171f2
baswanthd <61455583+baswanthd@users.noreply.github.com>
2023-12-29 14:41:10 +0100

    removed list in output.tf and commented access logs

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -1,9 +1,9 @@
 output "public_subnet_ids" {
-    value = ["${aws_subnet.public_subnets.*.id}"]
+    value = "${aws_subnet.public_subnets.*.id}"
 }
 
 output "private_subnet_ids" {
-    value = ["${aws_subnet.private_subnets.*.id}"]
+    value = "${aws_subnet.private_subnets.*.id}"
 }
 
 output "vpc_id" {
