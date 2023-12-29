a72a212f4d9c12bff728a24c590a632847e555c0
baswanthd <61455583+baswanthd@users.noreply.github.com>
2023-12-29 14:52:27 +0100

    modified output.tf syntax

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -4,9 +4,11 @@ output "public_subnet_ids" {
 }
 
 output "private_subnet_ids" {
-    value = "${aws_subnet.private_subnets.*.id}"
+    #value = "${aws_subnet.private_subnets.*.id}"
+    value = aws_subnet.private_subnets.*.id
 }
 
 output "vpc_id" {
-    value = "${aws_vpc.main_vpc.id}"
+    #value = "${aws_vpc.main_vpc.id}"
+    value = aws_vpc.main_vpc.id
 }
