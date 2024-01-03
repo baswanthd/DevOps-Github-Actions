892b1ab80253e6403c4189725d9a75a1e9d30f92
baswanth <baswanthd@gmail.com>
2024-01-03 15:56:54 +0100

    update output with load balancer ip and fargate ip

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,11 @@
 output "loadbalancer_dns-name" {
   value = aws_lb.bas_app_lb.dns_name
 }
+
+output "loadbalancer_public_ip" {
+  value = aws_lb.bas_app_lb.ip_address_type
+}
+
+output "fargate_ip" {
+  value = aws_ecs_service.bmi-bas8.network_configuration
+}
