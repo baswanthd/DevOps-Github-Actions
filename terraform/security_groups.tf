3feea8bd4f045b77a2e1c0f89d2a5640a93b35b9
baswanth <baswanthd@gmail.com>
2024-01-03 15:44:07 +0100

    updated load balancer sg with ip address

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "allow_everything" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["176.198.200.45/32"]
     #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
   }
 
