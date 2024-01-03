5ac683353b9a55d02301740dde450ef733df020e
baswanth <baswanthd@gmail.com>
2024-01-03 16:13:04 +0100

    update output.tf with dns_a_record_set" "lb_ips

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,10 +2,15 @@ output "loadbalancer_dns-name" {
   value = aws_lb.bas_app_lb.dns_name
 }
 
-output "loadbalancer_public_ip" {
+output "lb_ip_type" {
   value = aws_lb.bas_app_lb.ip_address_type
 }
 
 output "fargate_ip" {
   value = aws_ecs_service.bmi-bas8.network_configuration
+}
+
+data "dns_a_record_set" "lb_ips" {
+  host = aws_lb.bas_app_lb.dns_name
+
 }
