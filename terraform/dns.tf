eb26ed4bd1f99e122618a7e06025752c65e843bf
snoppiz <46250679+snoppiz@users.noreply.github.com>
2024-01-04 12:26:46 +0100

    add to new sub domain

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -9,3 +9,15 @@ resource "aws_route53_record" "test" {
   ttl     = 10
   records = data.dns_a_record_set.lb_ips.addrs
 }
+
+resource "aws_route53_zone" "second" {
+  name = "sub.baswanth-tech.de"
+}
+
+resource "aws_route53_record" "sub" {
+  zone_id = aws_route53_zone.second.zone_id
+  name    = "sub.baswanth-tech.de"
+  type    = "A"
+  ttl     = 10
+  records = data.dns_a_record_set.lb_ips.addrs
+}
