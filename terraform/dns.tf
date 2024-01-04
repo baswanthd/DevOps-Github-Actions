d4b6b1bc81c3121d4b71688dba440f10c8a0ab4b
snoppiz <46250679+snoppiz@users.noreply.github.com>
2024-01-04 11:39:56 +0100

    Update dns.tf
    added route 53 record

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,3 +1,11 @@
 resource "aws_route53_zone" "primary" {
   name = "test.baswanth-tech.de"
 }
+
+resource "aws_route53_record" "test" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "test.baswanth-tech.de"
+  type    = "A"
+  ttl     = 10
+  records = [aws_eip.bas_app_lb.public_ip]
+}
