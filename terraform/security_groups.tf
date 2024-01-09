d4167dc98e76be595a70c5a71ea6521d6cfb6fa1
baswanthd <61455583+baswanthd@users.noreply.github.com>
2024-01-09 14:35:00 +0100

    sg-updated

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -1,42 +1,15 @@
-resource "aws_security_group" "allow_everything" {
-  name        = "allow_everything"
-  description = "Allow everything"
-  vpc_id      = module.common_vpc.vpc_id
-
-  ingress {
-    description = "Allow-everything from VPC"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"] #["176.198.200.45/32"]
-    #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    #ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "allow_everything"
-  }
-}
-
-# resource "aws_security_group" "fargate-sg" {
-#   name        = "fargate-sg"
-#   description = "fargate-sg with limits"
+# resource "aws_security_group" "allow_everything" {
+#   name        = "allow_everything"
+#   description = "Allow everything"
 #   vpc_id      = module.common_vpc.vpc_id
 
 #   ingress {
-#     description     = "Allow-everything from VPC"
-#     from_port       = 0
-#     to_port         = 0
-#     protocol        = "-1"
-#     security_groups = [aws_security_group.allow_everything.id]
-
+#     description = "Allow-everything from VPC"
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"] #["176.198.200.45/32"]
+#     #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
 #   }
 
 #   egress {
@@ -44,9 +17,36 @@ resource "aws_security_group" "allow_everything" {
 #     to_port     = 0
 #     protocol    = "-1"
 #     cidr_blocks = ["0.0.0.0/0"]
+#     #ipv6_cidr_blocks = ["::/0"]
 #   }
 
 #   tags = {
-#     Name = "fargate-sg"
+#     Name = "allow_everything"
 #   }
 # }
+
+resource "aws_security_group" "fargate-sg" {
+  name        = "fargate-sg"
+  description = "fargate-sg with limits"
+  vpc_id      = module.common_vpc.vpc_id
+
+  ingress {
+    description     = "Allow-everything from VPC"
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = [aws_security_group.allow_everything.id]
+
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "fargate-sg"
+  }
+}
