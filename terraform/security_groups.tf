0c023482a922fe49fa23df39936fa756e907db2c
baswanthd <61455583+baswanthd@users.noreply.github.com>
2024-01-09 14:37:05 +0100

    sg-update

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -1,29 +1,29 @@
-# resource "aws_security_group" "allow_everything" {
-#   name        = "allow_everything"
-#   description = "Allow everything"
-#   vpc_id      = module.common_vpc.vpc_id
+resource "aws_security_group" "allow_everything" {
+  name        = "allow_everything"
+  description = "Allow everything"
+  vpc_id      = module.common_vpc.vpc_id
 
-#   ingress {
-#     description = "Allow-everything from VPC"
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"] #["176.198.200.45/32"]
-#     #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
-#   }
+  ingress {
+    description = "Allow-everything from VPC"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] #["176.198.200.45/32"]
+    #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#     #ipv6_cidr_blocks = ["::/0"]
-#   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    #ipv6_cidr_blocks = ["::/0"]
+  }
 
-#   tags = {
-#     Name = "allow_everything"
-#   }
-# }
+  tags = {
+    Name = "allow_everything"
+  }
+}
 
 resource "aws_security_group" "fargate-sg" {
   name        = "fargate-sg"
