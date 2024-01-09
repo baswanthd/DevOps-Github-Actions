c0d0540b78fbd3e488bcb0ef9ef48a1c9bbe7eae
baswanthd <61455583+baswanthd@users.noreply.github.com>
2024-01-09 17:27:50 +0100

    Update s3

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -7,7 +7,7 @@ data "aws_elb_service_account" "log_account"{}
 data "aws_caller_identity" "log_account_current" {}
 
 resource "aws_s3_bucket_policy" "log_data_policy" {
-  bucket = aws_s3_bucket.log_bucket.id
+  bucket = aws_s3_bucket.log-bucket-central.id
   policy = data.aws_iam_policy_document.log_data.json
 }
 
@@ -24,13 +24,13 @@ data "aws_iam_policy_document" "log_data" {
 
     resources = [
       #aws_s3_bucket.log_bucket.arn,
-      "${aws_s3_bucket.log_bucket-central.arn}/log/AWSLogs/${data.aws_caller_identity.log_account_current.account_id}/*",
+      "${aws_s3_bucket.log-bucket-central.arn}/log/AWSLogs/${data.aws_caller_identity.log_account_current.account_id}/*",
     ]
   }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "log_encryption" {
-  bucket = aws_s3_bucket.log_bucket-central.id
+  bucket = aws_s3_bucket.log-bucket-central.id
   rule {
     apply_server_side_encryption_by_default {
       # kms_master_key_id = aws_kms_key.mykey.arn # there is a default one
