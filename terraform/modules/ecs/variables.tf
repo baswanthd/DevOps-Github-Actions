feac5482d76ef8806ee752c9da9232b3decd740a
baswanthd <61455583+baswanthd@users.noreply.github.com>
2024-01-10 12:44:17 +0100

    fix

--- terraform/modules/ecs/variables.tf
+++ terraform/modules/ecs/variables.tf
@@ -10,7 +10,7 @@ variable "fargate_sg" {}
 
 variable "lb_target_group" {}
 
-variable "lb_listner" {}
+#variable "lb_listner" {}
 
 # variable "task_definition_arn" {
 #   default = aws_ecs_task_definition.bmi-service.arn
