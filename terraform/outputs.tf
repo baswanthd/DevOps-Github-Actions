f29816ed8461b81e141f8c82a4db2828d0eec10a
baswanthd <61455583+baswanthd@users.noreply.github.com>
2024-01-10 11:30:56 +0100

    commented lb in outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,22 +1,22 @@
-output "loadbalancer_dns-name" {
-  value = aws_lb.central-app-lb.dns_name
-}
+# output "loadbalancer_dns-name" {
+#   value = aws_lb.central-app-lb.dns_name
+# }
 
-output "lb_ip_type" {
-  value = aws_lb.central-app-lb.ip_address_type
-}
+# output "lb_ip_type" {
+#   value = aws_lb.central-app-lb.ip_address_type
+# }
 
-output "fargate_ip" {
-  value = aws_ecs_service.bmi-service.network_configuration
-}
+# output "fargate_ip" {
+#   value = aws_ecs_service.bmi-service.network_configuration
+# }
 
-data "dns_a_record_set" "lb_ips" {
-  host = aws_lb.central-app-lb.dns_name
-}
+# data "dns_a_record_set" "lb_ips" {
+#   host = aws_lb.central-app-lb.dns_name
+# }
 
-output "lb_public_ip" {
-  value = data.dns_a_record_set.lb_ips.addrs
-}
+# output "lb_public_ip" {
+#   value = data.dns_a_record_set.lb_ips.addrs
+# }
 
 output "vpc_id" {
   #value = "${aws_vpc.central_vpc.id}"
