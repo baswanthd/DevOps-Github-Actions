8ab9caf7e89380a7e028bc7631799c3ce4e7917f
baswanthd <61455583+baswanthd@users.noreply.github.com>
2024-01-11 11:54:53 +0100

    one service is uncommented

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,7 +34,7 @@ module "ecs" {
   subnets          = module.common_vpc.private_subnet_ids
   execution_role_arn = "arn:aws:iam::124937358799:role/ecsTaskExecutionRole"
   fargate_sg = aws_security_group.fargate-sg.id
-  # lb_target_group = aws_lb_target_group.central-app-tg.arn
+  lb_target_group = aws_lb_target_group.central-app-tg.arn
   # contact_form_tg = aws_lb_target_group.contact-form-tg.arn
  
  # lb_listner = aws_lb_listener.central-app-listner.arn
--- terraform/modules/ecs/ecs_service.tf
+++ terraform/modules/ecs/ecs_service.tf
@@ -14,30 +14,30 @@ resource "aws_ecs_service" "bmi-service" {
     assign_public_ip = false
   }
 
-  # load_balancer {
-  #   target_group_arn = var.lb_target_group
-  #   container_name   = "bmi"
-  #   container_port   = 8000
-  # }
+  load_balancer {
+    target_group_arn = var.lb_target_group
+    container_name   = "bmi"
+    container_port   = 8000
+  }
 }
 
-resource "aws_ecs_service" "contact-form" {
-  name            = "contact-form"
-  cluster         = aws_ecs_cluster.central-cluster.id
-  task_definition = aws_ecs_task_definition.contact-form-tf.arn
-  desired_count   = 1
-#   iam_role        = aws_iam_role.foo.arn
-#   depends_on      = [aws_iam_role_policy.foo]
-  launch_type     = "FARGATE"
+# resource "aws_ecs_service" "contact-form" {
+#   name            = "contact-form"
+#   cluster         = aws_ecs_cluster.central-cluster.id
+#   task_definition = aws_ecs_task_definition.contact-form-tf.arn
+#   desired_count   = 1
+# #   iam_role        = aws_iam_role.foo.arn
+# #   depends_on      = [aws_iam_role_policy.foo]
+#   launch_type     = "FARGATE"
 
-network_configuration {
-  subnets = var.subnets
-  security_groups = [var.fargate_sg]
-  assign_public_ip = false
- }
+# network_configuration {
+#   subnets = var.subnets
+#   security_groups = [var.fargate_sg]
+#   assign_public_ip = false
+#  }
 #  load_balancer {
 #     target_group_arn = var.contact_form_tg
 #     container_name   = "contact-form"
 #     container_port   = 8080
 #   }
-}
+# }
