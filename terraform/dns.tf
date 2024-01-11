40843f27e5973d790f1562e2bc3f1154a51940e0
baswanth <baswanthd@gmail.com>
2024-01-11 12:53:45 +0100

    syntax error

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -15,8 +15,8 @@ resource "aws_route53_record" "click-record" {
   #records = data.dns_a_record_set.lb_ips.addrs
 
     alias {
-    name                   = aws_elb.central-app-lb.dns_name
-    zone_id                = aws_elb.central-app-lb.zone_id
+    name                   = aws_lb.central-app-lb.dns_name
+    zone_id                = aws_lb.central-app-lb.zone_id
     evaluate_target_health = true
   }
 }
